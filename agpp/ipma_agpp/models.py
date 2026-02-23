04fab93649719e5f0d08da89bf84fee57059a671
Luis <iguanagamer9002@gmail.com>
2026-02-23 09:55:00 +0000

    [add]

--- agpp/ipma_agpp/models.py
+++ agpp/ipma_agpp/models.py
@@ -1,34 +1,35 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
+class User(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     cargo = models.CharField(max_length=100)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
+   # aplicacoes_ids = models.ManyToManyField(Aplicacoes)
 
-class user_passes(models.Model):
+class User_passes(models.Model):
     encriptacao = models.CharField(max_length=100)
     data_ultima_atualizacao = models.DateTimeField(auto_now=True)
     expira_em = models.DateTimeField()
 
-class aplicacoes(models.Model):
+class Aplicacoes(models.Model):
     nome = models.CharField(max_length=100)
     versoes = models.CharField(max_length=100)
     fornecedores = models.CharField(max_length=100)
 
-class registo_de_aplicacoes (models.Model):
+class Registo_de_aplicacoes (models.Model):
     data = models.DateTimeField(auto_now_add=True)
 
-class equipamento(models.Model):
+class Equipamento(models.Model):
     nome = models.CharField(max_length=100)
     numero_inventario = models.CharField(max_length=100)
     numero_serie = models.CharField(max_length=100)
 
-class registo_equipamento(models.Model):
+class Registo_equipamento(models.Model):
     data = models.DateTimeField(auto_now_add=True)
 
-class logs(models.Model):
+class Logs(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     observacoes = models.CharField(max_length=200)
--- agpp/ipma_agpp/views.py
+++ agpp/ipma_agpp/views.py
@@ -4,3 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def user_list(request):
     return render(request, 'ipma_agpp/user_list.html')
+
