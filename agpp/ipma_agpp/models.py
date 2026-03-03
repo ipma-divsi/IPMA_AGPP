1c373d83aa6249c766cf863ff55f65927e9d344f
ana <anaclaragoncalvesalves07@gmail.com>
2026-03-03 12:22:28 +0000

    [ADD] - Atualizações no settings e no  models

--- agpp/ipma_agpp/models.py
+++ agpp/ipma_agpp/models.py
@@ -1,35 +1,59 @@
+
 from django.db import models
 
-# Create your models here.
+
 class User(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     cargo = models.CharField(max_length=100)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
-   # aplicacoes_ids = models.ManyToManyField(Aplicacoes)
+
 
 class User_passes(models.Model):
     encriptacao = models.CharField(max_length=100)
     data_ultima_atualizacao = models.DateTimeField(auto_now=True)
     expira_em = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Aplicacoes(models.Model):
     nome = models.CharField(max_length=100)
     versoes = models.CharField(max_length=100)
     fornecedores = models.CharField(max_length=100)
+    nomes = models.ManyToManyField(User, through='User_aplicacoes')
 
-class Registo_de_aplicacoes (models.Model):
+
+class User_aplicacoes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aplicacao = models.ForeignKey(Aplicacoes, on_delete=models.CASCADE)
+    data_inicio = models.DateField(auto_now_add=True)
+    data_fim = models.DateField(auto_now_add=True)
+    atribuido_por = models.CharField(max_length=100)
+
+
+class Registo_de_aplicacoes(models.Model):
     data = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aplicacao = models.ForeignKey(Aplicacoes, on_delete=models.CASCADE)
+
 
 class Equipamento(models.Model):
     nome = models.CharField(max_length=100)
     numero_inventario = models.CharField(max_length=100)
     numero_serie = models.CharField(max_length=100)
+    aplicacao = models.ForeignKey(Aplicacoes, on_delete=models.CASCADE)
+
 
 class Registo_equipamento(models.Model):
     data = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aplicacao = models.ForeignKey(Aplicacoes, on_delete=models.CASCADE)
+
 
 class Logs(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     observacoes = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    logs_aplicacoes = models.ManyToManyField(Aplicacoes)
+    logs_equipamentos = models.ManyToManyField(Equipamento)
